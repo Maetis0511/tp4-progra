fdd155ea03ba1010743b182b2b44cd01faa622c9
Maetis <41517613+Maetis0511@users.noreply.github.com>
2024-01-08 11:37:36 +0100

    question 2.12

--- src/app/mon-compte/layout.tsx
+++ src/app/mon-compte/layout.tsx
@@ -1,37 +1,45 @@
-import { ReactNode } from "react";
+import {ReactNode} from "react";
 import {Card, SectionContainer} from "tp-kit/components";
 import prisma from "../../utils/prisma";
-import { OrderTable } from "../../components/order-table";
+import {OrderTable} from "../../components/order-table";
+import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
+import { cookies } from 'next/headers';
 
-export default async function Layout({ children }: { children: ReactNode }) {
-  const orders = await prisma.order.findMany();
+export default async function Layout({children}: { children: ReactNode }) {
+    const supabase = createServerComponentClient({ cookies });
+    const {data} = await supabase.auth.getUser();
 
-  return (
-    <div
-        className="flex"
-    >
+    const orders = await prisma.order.findMany({
+        where: {
+            userId: data.user?.id
+        }
+    });
 
-      <div
-        className="basis-2/6 bg-coffee-50 py-8 lg:px-8"
-      >
+    return (
         <div
-            className="py-24 min-h-[80vh]"
+            className="flex"
         >
-          <Card
-              className="w-full py-4"
-          >
-            {children}
-          </Card>
-        </div>
-      </div>
-      {/* Orders list */}
-      <SectionContainer wrapperClassName="py-24 min-h-[80vh]" className="basis-4/6">
-        <div className="bg-white rounded-lg p-6 shadow-lg">
-          <OrderTable orders={orders} />
-        </div>
-      </SectionContainer>
+            <div
+                className="basis-2/6 bg-coffee-50 py-8 lg:px-8"
+            >
+                <div
+                    className="py-24 min-h-[80vh]"
+                >
+                    <Card
+                        className="w-full py-4"
+                    >
+                        {children}
+                    </Card>
+                </div>
+            </div>
+            {/* Orders list */}
+            <SectionContainer wrapperClassName="py-24 min-h-[80vh]" className="basis-4/6">
+                <div className="bg-white rounded-lg p-6 shadow-lg">
+                    <OrderTable orders={orders}/>
+                </div>
+            </SectionContainer>
 
-      {/* Children */}
-    </div>
-  );
+            {/* Children */}
+        </div>
+    );
 }
