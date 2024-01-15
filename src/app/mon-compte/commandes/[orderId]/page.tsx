b96ce332cbd7a5645f15fa890b3264d8fe960f98
Maetis <41517613+Maetis0511@users.noreply.github.com>
2024-01-15 14:35:10 +0100

    question 3.2

--- src/app/mon-compte/commandes/[orderId]/page.tsx
+++ src/app/mon-compte/commandes/[orderId]/page.tsx
@@ -2,6 +2,8 @@ import { OrderDetailsLayout } from "tp-kit/components";
 import { NextPageProps } from "../../../../types";
 import prisma from "../../../../utils/prisma";
 import { notFound } from "next/navigation";
+import {createClientComponentClient} from "@supabase/auth-helpers-nextjs";
+import RealTimeOrderDetails from "../../../../components/RealTimeOrderDetails";
 
 type Props = {
   orderId: string;
@@ -18,7 +20,23 @@ export default async function OrderDetailsPage({params}: NextPageProps<Props>) {
     }
   });
 
+  const supabase = createClientComponentClient();
+
+  const channel = supabase
+      .channel('update-order')
+      .on(
+          'postgres_changes',
+          {
+            event: 'UPDATE',
+            schema: 'public',
+          },
+          (payload) => console.log(payload)
+      )
+      .subscribe()
+
   if (!order) notFound();
 
-  return <OrderDetailsLayout order={order} />
+  return (
+      <RealTimeOrderDetails order={order} />
+  )
 }
